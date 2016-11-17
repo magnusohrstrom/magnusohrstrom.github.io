860734065d058381fe56f1553212e57ab8ebc7b0
magnusohrstrom <magnusohrstrom330@gmail.com>
2016-11-17 11:05:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,203 +1,72 @@
-//Magnus Todo-List!
+/////////////Magnus Foodgenerator////////////////////////
 
-///////////Struktur html och css.//////////////////////////////////////
-var center = document.createElement('center');
-document.body.appendChild(center);
+///////////////////////Creates array of objects/////////////////////////
+var arrCourses = [];
 
-
-document.body.style.backgroundColor = "#f2f2f2";
-
-//Låda för rubrik.
-var divH1 = document.createElement('header');
-divH1.setAttribute('id', "divH1");
-divH1.style.backgroundColor = "#003366";
-divH1.style.display = "block";
-divH1.style.height = "70px";
-divH1.style.width = "660px";
-divH1.style.padding= "10px";
-divH1.style.margin = "10px";
-center.appendChild(divH1);
-
-
-//Div för inputruta och knapp.
-var divInput = document.createElement('div');
-divInput.style.display="block";
-divInput.style.height = "70px";
-divInput.style.width = "660px";
-divInput.style.margin = "10px";
-divInput.style.border = "0";
-divInput.style.padding = "10px";
-divInput.style.backgroundColor= "salmon";
-center.appendChild(divInput);
-
-//textruta
-var form = document.createElement('form');
-form.setAttribute('id','form');
-divInput.appendChild(form);
-
-//Knapp som lägger till texten i Todolistan.
-var buttonAdd = document.createElement('button');
-buttonAdd.type = "submit";
-buttonAdd.style.backgroundColor="white";
-buttonAdd.style.fontFamily= "Courier New, monospace";
-buttonAdd.style.border= "0";
-buttonAdd.style.margin="0";
-buttonAdd.style.width="120px";
-var textButtonAdd = document.createTextNode('Add to list');
-divInput.appendChild(buttonAdd);
-buttonAdd.appendChild(textButtonAdd);
-buttonAdd.addEventListener("click", addTodo);
-
-
-//Rubrik.
-var h1 = document.createElement('h2');
-var textH1 = document.createTextNode('My Todo-List');
-divH1.style.color = "white";
-divH1.style.fontFamily = "Courier New, monospace";
-h1.appendChild(textH1);
-divH1.appendChild(h1);
-
-//Div som samlar de båda listDivsen.
-var divMain = document.createElement('div');
-divMain.style.height = "100%";
-divMain.style.width = "700px";
-divMain.style.margin = "30px";
-
-
-center.appendChild(divMain);
-
-//Låda för Todolist vänster.
-var divTodo = document.createElement('div');
-divTodo.setAttribute('id', 'divTodo');
-divTodo.style.backgroundColor = "#5c85d6";
-divTodo.style.height = "100%";
-divTodo.style.width = "300px";
-divTodo.style.float = "left";
-divTodo.style.margin="10px";
-divTodo.style.padding = "10px";
-divTodo.style.fontFamily = "Courier New, monospace";
-divTodo.style.color = "white";
-divTodo.style.textAlign = "center";
-divMain.appendChild(divTodo);
-var h3divTodo = document.createElement('h3');
-var textdivTodo = document.createTextNode('Things to do:');
-h3divTodo.appendChild(textdivTodo);
-divTodo.appendChild(h3divTodo);
-
-
-//Låda för färdiga events.
-var divDone = document.createElement('div');
-divDone.setAttribute('id', 'divDone');
-divDone.style.backgroundColor = "grey";
-divDone.style.height = "100%";
-divDone.style.width = "300px";
-divDone.style.float = "right";
-divDone.style.padding = "10px";
-divDone.style.margin = "10px";
-divDone.style.fontFamily = "Courier New, monospace";
-divDone.style.color = "white";
-divMain.appendChild(divDone);
-var h3divDone = document.createElement('h3');
-var textdivDone = document.createTextNode('Done:');
-h3divDone.appendChild(textdivDone);
-divDone.appendChild(h3divDone);
-
-//Lista där att göra skrivs.
-var list = document.createElement('ul');
-list.style.textAlign="left";
-list.style.fontSize="14px";
-divTodo.appendChild(list);
-//Lista där färiga tasks skrivs.
-var listDone = document.createElement('ul');
-listDone.style.textAlign="left";
-listDone.style.fontSize="14px";
-divDone.appendChild(listDone);
-
-
-
-
-//FUNKTIONALITET//////////////////////////////////////////////////////////////////
-//Ruta att skriva in task i.
-var input = document.createElement('input');
-input.setAttribute('id', 'inputId');
-input.setAttribute('placeholder', 'Enter task here.');
-input.style.clear= "both";
-input.style.textAlign = "center";
-input.type = "text";
-input.size = "20";
-//input.addEventListener("keypress",enterKey);
-form.appendChild(input);
-
-////////FUNCTIONER//////////
-
-//Adderar li element med knappar till Todo-List.
-function addTodo() {
-
-    var userInput = document.getElementById("inputId").value;
-    if(userInput.trim()===""){
-      alert("You didn't add anything.");
-    }
-    else{
-        var li = document.createElement("li");
-        li.setAttribute('class', 'li');
-
-        var chore = document.createTextNode(userInput);
-        var p = document.createElement('p');
-        p.style.wordWrap = "break-word";
-        p.appendChild(chore);
-        li.appendChild(p);
-        list.appendChild(li);
-
-        addCloseButton(p);
-        addDoneButton(p);
-
-    }
+//Course constructor.
+function Course(str, isVeggie, url) {
+    this.str = str;
+    this.isVeggie = isVeggie;
+    this.url = url;
 }
+//Adds objectet Course to courseArr.
+function addCourse(course) {
+    arrCourses.push(course);
+}
+//Courses objects are pushed to courseArr.
+addCourse(kottbullar=new Course("Köttbullar med potatismos, gräddsås, lingon och stektlök.", false,"https://www.landleyskok.se/recept/hemlagade-kottbullar.html/"));
+addCourse(kolbulle = new Course("Grillad kolbulle med lingonsylt.", false, "http://www.koket.se/sommar-med-ernst/ernst-kirchsteiger/kolbulle-pa-ernsts-vis/"));
+addCourse(springroll= new Course("Vårrulle med sötsursås.", true, "http://www.ica.se/recept/farska-varrullar-med-grillad-tofu-och-ingefarsdipp-720099/"));
+addCourse(haloumi= new Course("Haloumisallad", true, "http://mittkok.expressen.se/recept/halloumisallad/"));
+addCourse(Baconinlindad=new Course("Baconinlindad kycklingfile.",false,"https://www.kokaihop.se/recept/baconinlindad-kycklingfile-2"));
+addCourse(lasagne=new Course("Lasagne med köttfärssås.",false,"http://www.koket.se/lasagne-med-kottfarssas"));
+addCourse(majs=new Course("Majsplättar med avokadoröra.",true,"https://www.mathem.se/recept/mattias-majsplattar-med-avokado-chimichurri"));
+addCourse(soppachori=new Course("Grönkålssoppa med chorizo.",false,"http://www.matklubben.nu/recept/gr%F6nk%E5lssoppa_med_chorizo_122704.html#.WCxTNcn3b9I"));
+addCourse(pasta=new Course("Pastagratäng med rökt lax och spenat.",false,"www.ica.se/recept/pastagratang-med-rokt-lax-och-spenat-719271/"));
+addCourse(thaigryta=new Course("Thaigryta med kyckling.",false,"http://www.koket.se/mitt-kok/donal-skehan/thaigryta-med-kyckling/"));
+addCourse(lax=new Course("Lax med nudelsallad.",false,"https://www.mathem.se/recept/gustavs-sesamstekta-lax-med-varm-nudelsallad--lime-och-koriander"));
+addCourse(lasagnehall=new Course("Lasagne med halloumi, spenat och pumpakärnor.",true,"www.ica.se/recept/lasagne-med-halloumi-spenat-och-pumpakarnor-718382/"));
+addCourse(risotto=new Course("Rödbetsrisotto med hyvlad halloumi och valnötter.",true,"www.ica.se/recept/rodbetsrisotto-med-hyvlad-halloumi-och-valnotter-720045/"));
+addCourse(tonfisk=new Course("Halstrad tonfisk med avokadohummus.",false,"www.ica.se/recept/halstrad-tonfisk-med-avokadohummus-719975/"));
+addCourse(skav=new Course("Viltskavsgryta med mandelpotatispuré & rårörda lingon.",false,"https://www.mathem.se/recept/viltskavsgryta-med-mandelpotatispure-o-rarorda-lingon"));
+addCourse(coq=new Course("Coq Au Vin.",false,"http://www.foodnetwork.com/recipes/ina-garten/coq-au-vin-recipe4.html"));
+addCourse(dahl=new Course("Indisk linssoppa ”Mulligatawny”.",true,"http://www.ica.se/recept/indisk-linssoppa-mulligatawny-718554/"));
+addCourse(Falafel=new Course("Falafel med hummus, yoghurtdressing och picklad rödlök.",true,"http://www.ica.se/recept/falafel-med-hummus-yoghurtdressing-och-picklad-rodlok-717166/"));
+addCourse(pastag=new Course("Vegetarisk pastagratäng.",true,"https://www.landleyskok.se/recept/vegetarisk-pastagratang.html/"));
+/*
+addCourse(=new Course("",,""));
+addCourse(=new Course("",,""));
+addCourse(=new Course("",,""));
+addCourse(=new Course("",,""));
+addCourse(=new Course("",,""));
+*/
+//////////////////////////Functionality//////////////////////////////////
+//Button to generate.
+var button = document.getElementById('button');
 
-//Funktion för submit på Enter-key.
-function enterKey(event){
-  if(window.event.keyCode == '13'){
-    addTodo();
+button.addEventListener("click", generateRandomCourse);
+
+
+//Function for button to gererate a course-log.
+function generateRandomCourse() {
+  var checkBoxValue = document.getElementById('checkbox').checked;
+  var a = document.getElementById('courseP');
+//If user is not veggie. Gets random object from arrCourses.
+  if(checkBoxValue === false) {
+    var i = Math.floor(Math.random() * arrCourses.length);
+    a.innerHTML = arrCourses[i].str;
+    a.href = arrCourses[i].url;
   }
-}
-
-//Funktion som lägger till closeknapp.
-function addCloseButton(x){
-  var closeButton = document.createElement("button");
-  closeButton.setAttribute('class', 'closeButton');
-  var closeButtonText = document.createTextNode('Remove');
-  closeButton.style.border = "0";
-  closeButton.style.margin = "2px";
-  closeButton.style.backgroundColor="white";
-  closeButton.style.fontFamily="Courier New, monospace";
-  closeButton.style.float="right";
-  closeButton.appendChild(closeButtonText);
-  x.appendChild(closeButton);
-  closeButton.addEventListener('click', close);
-}
-
-//Skapa Done-knapp.
-function addDoneButton(x){
-  var buttonDone = document.createElement('button');
-  buttonDone.setAttribute('class', 'buttonDone');
-  var buttonDoneText = document.createTextNode('Done');
-  buttonDone.style.backgroundColor="white";
-  buttonDone.style.margin="2px";
-  buttonDone.style.border="0";
-  buttonDone.style.fontFamily="Courier New, monospace";
-  buttonDone.style.float="right";
-  buttonDone.appendChild(buttonDoneText);
-  x.appendChild(buttonDone);
-  buttonDone.addEventListener('click', doneMove);
-}
-
-//Tar bort li från Todo och lägger till det i Done-listan.
-function doneMove() {
-    listDone.appendChild(this.parentNode.parentNode);
-    this.parentNode.style.textDecoration="line-through";
-    this.parentNode.removeChild(this);
-}
-//funktion som tar bort li elementet från todo.
-function close(){
-  this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+  //If user checks
+  else if(checkBoxValue === true){
+      var loop = true;
+      while(loop){
+      var j = Math.floor(Math.random() * arrCourses.length);
+      if(arrCourses[j].isVeggie){
+        a.innerHTML = arrCourses[j].str;
+        a.href = arrCourses[j].url;
+      }
+      loop=false;
+    }
+  }
 }
