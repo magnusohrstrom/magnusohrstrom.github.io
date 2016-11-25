5182af2974058d0805c7922ee2198b4d61105a59
magnusohrstrom <magnusohrstrom@Magnuss-MacBook-Pro-2.local>
2016-11-25 16:52:31 +0100

    Final innan inlämning1

--- Foodgenerator/script.js
+++ Foodgenerator/script.js
@@ -22,12 +22,12 @@ addCourse(Baconinlindad=new Course("Baconinlindad kycklingfile.",false,"https://
 addCourse(lasagne=new Course("Lasagne med köttfärssås.",false,"http://www.koket.se/lasagne-med-kottfarssas"));
 addCourse(majs=new Course("Majsplättar med avokadoröra.",true,"https://www.mathem.se/recept/mattias-majsplattar-med-avokado-chimichurri"));
 addCourse(soppachori=new Course("Grönkålssoppa med chorizo.",false,"http://www.matklubben.nu/recept/gr%F6nk%E5lssoppa_med_chorizo_122704.html#.WCxTNcn3b9I"));
-addCourse(pasta=new Course("Pastagratäng med rökt lax och spenat.",false,"www.ica.se/recept/pastagratang-med-rokt-lax-och-spenat-719271/"));
+addCourse(pasta=new Course("Pastagratäng med rökt lax och spenat.",false,"http://www.ica.se/recept/pastagratang-med-rokt-lax-och-spenat-719271/"));
 addCourse(thaigryta=new Course("Thaigryta med kyckling.",false,"http://www.koket.se/mitt-kok/donal-skehan/thaigryta-med-kyckling/"));
 addCourse(lax=new Course("Lax med nudelsallad.",false,"https://www.mathem.se/recept/gustavs-sesamstekta-lax-med-varm-nudelsallad--lime-och-koriander"));
-addCourse(lasagnehall=new Course("Lasagne med halloumi, spenat och pumpakärnor.",true,"www.ica.se/recept/lasagne-med-halloumi-spenat-och-pumpakarnor-718382/"));
-addCourse(risotto=new Course("Rödbetsrisotto med hyvlad halloumi och valnötter.",true,"www.ica.se/recept/rodbetsrisotto-med-hyvlad-halloumi-och-valnotter-720045/"));
-addCourse(tonfisk=new Course("Halstrad tonfisk med avokadohummus.",false,"www.ica.se/recept/halstrad-tonfisk-med-avokadohummus-719975/"));
+addCourse(lasagnehall=new Course("Lasagne med halloumi, spenat och pumpakärnor.",true,"http://www.ica.se/recept/lasagne-med-halloumi-spenat-och-pumpakarnor-718382/"));
+addCourse(risotto=new Course("Rödbetsrisotto med hyvlad halloumi och valnötter.",true,"http://www.ica.se/recept/rodbetsrisotto-med-hyvlad-halloumi-och-valnotter-720045/"));
+addCourse(tonfisk=new Course("Halstrad tonfisk med avokadohummus.",false,"http://www.ica.se/recept/halstrad-tonfisk-med-avokadohummus-719975/"));
 addCourse(skav=new Course("Viltskavsgryta med mandelpotatispuré & rårörda lingon.",false,"https://www.mathem.se/recept/viltskavsgryta-med-mandelpotatispure-o-rarorda-lingon"));
 addCourse(coq=new Course("Coq Au Vin.",false,"http://www.foodnetwork.com/recipes/ina-garten/coq-au-vin-recipe4.html"));
 addCourse(dahl=new Course("Indisk linssoppa ”Mulligatawny”.",true,"http://www.ica.se/recept/indisk-linssoppa-mulligatawny-718554/"));
@@ -61,9 +61,9 @@ function generateRandomCourse() {
   if(checkBoxValue === false) {
     var i = randomIndex();
     a.innerHTML = arrCourses[i].str;
-    a.href = arrCourses[i].url;;
+    a.href = arrCourses[i].url;
   }
-  //If user checks
+  //If user checks box then only veggo alternatives is returned.
   else if(checkBoxValue === true){
       var loop = true;
       while(loop){
@@ -95,22 +95,25 @@ function setStyle1(){
   document.getElementById('mainDiv').style.backgroundColor="";
   document.getElementById('mainDiv').style.color="";
   document.getElementById('courseP').style.color="";
+  document.getElementById('xl').style.background="";
 
 }
 function setStyle2(){
   document.getElementById('header').style.backgroundColor="#ecf0f1";
-  document.getElementById('button').style.background="linear-gradient(#5989A8,#2c3e50)";
+  document.getElementById('button').style.background="linear-gradient(#4C5F8D,#0A2463)";
   document.getElementById('header').style.color="#2c3e50";
-  document.getElementById('mainDiv').style.backgroundColor="#e74c3c";
-  document.getElementById('mainDiv').style.color="white";
-  document.getElementById('courseP').style.color="white";
+  document.getElementById('mainDiv').style.backgroundColor="#9B1D20";
+  document.getElementById('mainDiv').style.color="#E2E2E2";
+  document.getElementById('courseP').style.color="#E2E2E2";
+  document.getElementById('xl').style.background="#ecf0f1";
 
 }
 function setStyle3(){
-  document.getElementById('header').style.backgroundColor="#FBD38D";
+  document.getElementById('header').style.backgroundColor="#1A9CA5";
   document.getElementById('button').style.background="linear-gradient(#FF6363,#c0392b)";
-  document.getElementById('header').style.color="#2c3e50";
-  document.getElementById('mainDiv').style.backgroundColor="#2c3e50";
-  document.getElementById('mainDiv').style.color="#FBD38D";
-  document.getElementById('courseP').style.color="#FBD38D";
+  document.getElementById('header').style.color="#FFF3DD";
+  document.getElementById('mainDiv').style.backgroundColor="#FFF3DD";
+  document.getElementById('mainDiv').style.color="#172B5B";
+  document.getElementById('courseP').style.color="#172B5B";
+  document.getElementById('xl').style.background="#1A9CA5";
 }
