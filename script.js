3d0ed8ef98712d15d06fc3e0a033de630544e2cc
magnusohrstrom <magnusohrstrom330@gmail.com>
2016-11-28 18:57:57 +0100

    hihi

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var projects= document.getElementById("projects");
 projects.addEventListener("click",showProjects);
 
 function showProjects(){
+
   var divProjects = document.createElement('div');
   divProjects.setAttribute('id','divProjects');
 
@@ -26,7 +27,31 @@ function showProjects(){
   a3.innerHTML = "In progress..";
   a3.setAttribute('class','dropdownA');
   divProjects.appendChild(a3);
+  
+}
 
+var nackademin = document.getElementById('divProjects');
+nackademin.addEventListener('click',showSchool);
+function showSchool(){
+  var schoolProjects=document.createElement('div');
+  schoolProjects.setAttribute('id','divSchool');
 
+  var a1 = document.createElement('a');
+  a1.setAttribute('href','Foodgenerator/index.html');
+  a1.innerHTML = "Receptgenerator";
+  a1.setAttribute('class','dropdownA');
+  divProjects.appendChild(a1);
+
+  var a2 = document.createElement('a');
+  a2.setAttribute('href','todo/index.html');
+
+  a2.innerHTML = "your Todo-list";
+  a2.setAttribute('class','dropdownA');
+  divProjects.appendChild(a2);
+
+  var a3 = document.createElement('a');
+  a3.innerHTML = "In progress..";
+  a3.setAttribute('class','dropdownA');
+  divProjects.appendChild(a3);
 
 }
